8c983843b76dbabfe82479d4682fb595607c7f24
DJ <152664227+DJ-KOMY-DIG@users.noreply.github.com>
2025-08-19 12:00:10 +0900

    change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const checkbox = document.getElementById('check');
-const copyValue = "file:///C:/Users/3169885/OneDrive%20-%20%E3%83%88%E3%83%A8%E3%82%BF%E8%87%AA%E5%8B%95%E8%BB%8A%E6%A0%AA%E5%BC%8F%E4%BC%9A%E7%A4%BE/02_Projects/14_JavaScript/ClickFix/werning.html";
+const copyValue = "https://dj-komy-dig.github.io/warning/";
 
 checkbox.addEventListener('change', function() {
     // チェック状態を確認して処理を分岐
